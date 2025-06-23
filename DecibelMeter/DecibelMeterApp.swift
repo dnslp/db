32558392b92e85e556303e45ccc21493a347379f
dnslp <davidnymanslp@gmail.com>
2025-06-23 12:50:07 -0400

    Initial Commit

---
+++ DecibelMeter/DecibelMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecibelMeterApp.swift
+//  DecibelMeter
+//
+//  Created by David Nyman on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct DecibelMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DecibelMeterTests/DecibelMeterTests.swift
@@ -0,0 +1,17 @@
+//
+//  DecibelMeterTests.swift
+//  DecibelMeterTests
+//
+//  Created by David Nyman on 6/23/25.
+//
+
+import Testing
+@testable import DecibelMeter
+
+struct DecibelMeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
